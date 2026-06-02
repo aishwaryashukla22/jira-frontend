ece056cd87929f459e1ff79400d8bc0a025828a6
Aishwarya Shukla <aishwaryashukla8@gmail.com>
2026-06-02 17:47:37 +0530

    final changes (#2)
    Co-authored-by: Aishwarya shukla <aishwaryashukla@Aishwaryas-MacBook-Air-2.local>

--- src/pages/jira-board/JiraBoard.tsx
+++ src/pages/jira-board/JiraBoard.tsx
@@ -3,12 +3,13 @@ import StatusColumns from '../../components/StatusColumn';
 import Modal from '../../components/Modal';
 import CreateTicketForm from '../../components/CreateTicketForm';
 import type { TicketFormData } from '../../components/CreateTicketForm';
+import { BASE_URL } from '../../contants';
 
 export default function JiraBoard() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [tickets, setTickets] = useState([])
     function getTickets() {
-        fetch('http://localhost:3000/api/tickets')
+        fetch(`${BASE_URL}/api/tickets`)
             .then(res => {
                 return res.json()
             })
@@ -21,7 +22,7 @@ export default function JiraBoard() {
         getTickets()
     }, [])
     async function createTicket(data: TicketFormData) {
-        const response = await fetch('http://localhost:3000/api/tickets', {
+        const response = await fetch(`${BASE_URL}/api/tickets`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -41,7 +42,7 @@ export default function JiraBoard() {
             <div className='text-right m-4'>
                 <button className='flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500' onClick={() => setIsModalOpen(true)}>+ Create New Ticket</button>
             </div>
-            {tickets && <StatusColumns cards={tickets} />}
+            {tickets && <StatusColumns cards={tickets} onUpdate={getTickets} />}
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title="Create New Ticket">
                 <CreateTicketForm onSubmit={createTicket} onCancel={() => setIsModalOpen(false)} />
             </Modal>
